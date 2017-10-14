18bf9c93b6584a44ba061c6cd5bf726a15922872
rhumbaflappy <32782457+rhumbaflappy@users.noreply.github.com>
2017-10-14 12:16:08 -0500

    Add files via upload

---
+++ OpenStreetMapServer/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("OpenStreetMapServer")]
+[assembly: Guid("1c336d16-cc76-44d1-86d5-6c4c603f2008")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("OpenStreetMapServer")]
+[assembly: AssemblyVersion("1.0.0.0")]
